81ab05078d41731612e0512824b4ee0b4795f3d3
Adriana <adrialvarez07.aa@gmail.com>
2024-05-07 16:48:09 -0500

    AdelantoFront_7/05/24

--- Actividades.js
+++ Actividades.js
@@ -9,7 +9,29 @@ function irAPaginaSesion() {
 function irAPaginaCrear() {
   window.location.href = "Registro.html";
 }
+function irAPaginaContraseña() {
+  window.location.href = "OlvideContra.html";
+}
+function irAPaginaPerfil() {
+  window.location.href = "Perfil.html";
+}
+function irAPaginaCod1() {
+  window.location.href = "Cod1.html";
+}
 function cerrarVentana() {
   var ventana = document.getElementById("ventana-Signin");
   ventana.style.display = "none"; // Ocultar la ventana emergente
+}
+
+// revisar funcion, resisar si es necesaria
+function validarFormulario() {
+  var nombre = document.getElementById("nombre").value;
+  var email = document.getElementById("email").value;
+
+  if (nombre === "" || email === "") {
+      alert("Por favor, completa todos los campos.");
+      return false; // Evita que el formulario se envíe
+  }
+
+  return true; // Permite que el formulario se envíe
 }
